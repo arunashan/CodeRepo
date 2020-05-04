c92a744ab8779600af5f8eabc247ae4b7af861de
arunashan <arunashanubhogue@gmail.com>
2020-05-04 18:42:22 +0530

    converted as war

---
+++ ProductManager/target/ProductManager-0.0.1-SNAPSHOT/WEB-INF/classes/config.js
@@ -0,0 +1,8 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_imxvRDAS8', // e.g. us-east-2_uXboG5pAb
+        region: 'us-east-1', // e.g. us-east-2
+		clientId: '7okvu7b3kc1p1dvon1jb16g1rf' //is this used anywhere?
+    },
+};
+
